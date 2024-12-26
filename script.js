1683a6aad2b29729a9eff146150904c09f176fe7
Максим <141343669+Gustav6dbjd@users.noreply.github.com>
2024-12-26 11:27:03 +0300

    default

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+document.body.getElementsByClassName('sbc')[0].style.backgroundColor = 'red';
+
+
+let styles = `
+    display: flex;
+    justify-content: space-between;
+   background-color: red;
+`
+
+document.body.getElementsByClassName("nonfication")[0].style = styles;
+
+
